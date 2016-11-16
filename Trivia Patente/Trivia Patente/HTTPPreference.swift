27ea4520f414cb2b2fb72e1d6ae55070aae7addb
Luigi Donadel <luigi.donadel@gmail.com>
2016-11-16 13:45:36 +0100

    user preferences are now saved on memory

--- Trivia Patente/Trivia Patente/HTTPPreference.swift
+++ Trivia Patente/Trivia Patente/HTTPPreference.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class HTTPPreference: HTTPManager {
     func change_notifications(key : String, value : Bool, handler : @escaping (TPPreferenceChangeResponse) -> Void) {
-        self.request(url: "/preferences/notification/\(key)/edit", method: .post, params: ["new_value": value], handler: handler)
+        self.request(url: "/preferences/notification/\(key)/edit", method: .post, params: ["new_value": value]) { (response : TPPreferenceChangeResponse) in
+            if response.success == true {
+                Shared.preferences = response.preferences
+            }
+            handler(response)
+        }
     }
     func change_others(key : String, value : PreferenceVisibility, handler : @escaping (TPPreferenceChangeResponse) -> Void) {
-        self.request(url: "/preferences/\(key)/edit", method: .post, params: ["new_value": value.rawValue], handler: handler)
+        self.request(url: "/preferences/\(key)/edit", method: .post, params: ["new_value": value.rawValue]) { (response : TPPreferenceChangeResponse) in
+            if response.success == true {
+                Shared.preferences = response.preferences
+            }
+            handler(response)
+        }
     }
 }
