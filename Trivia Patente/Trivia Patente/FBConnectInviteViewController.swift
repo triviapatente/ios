0e703a0181a30748336967eb9081df3ed7af27fd
Luigi Donadel <luigi.donadel@gmail.com>
2016-11-27 22:36:01 +0100

    added connect with facebook view invite

---
+++ Trivia Patente/Trivia Patente/FBConnectInviteViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FBConnectInviteViewController.swift
+//  Trivia Patente
+//
+//  Created by Luigi Donadel on 27/11/16.
+//  Copyright © 2016 Terpin e Donadel. All rights reserved.
+//
+
+import UIKit
+
+class FBConnectInviteViewController: UIViewController {
+
+    @IBOutlet var exitButton : UIButton!
+    @IBAction func connect() {
+        print("connect")
+    }
+    @IBAction func dismiss() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    var canDismiss : Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.smallRounded()
+        self.exitButton.isHidden = !canDismiss
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
