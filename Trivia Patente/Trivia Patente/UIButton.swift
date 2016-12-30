5e19059d1efe402f9a09d9f3c9d59d12d08724f4
Luigi Donadel <luigi.donadel@gmail.com>
2016-12-30 23:03:29 +0100

    added pressed state for every button

---
+++ Trivia Patente/Trivia Patente/UIButton.swift
@@ -0,0 +1,28 @@
+//
+//  sdf.swift
+//  Trivia Patente
+//
+//  Created by Luigi Donadel on 30/12/16.
+//  Copyright © 2016 Terpin e Donadel. All rights reserved.
+//
+
+import UIKit
+
+let DARK_OFFSET = CGFloat(0.05)
+
+extension UIButton {
+
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let color = self.backgroundColor {
+            self.backgroundColor = color.darker(offset: DARK_OFFSET)
+        }
+        super.touchesBegan(touches, with: event)
+    }
+    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let color = self.backgroundColor {
+            self.backgroundColor = color.darker(offset: -DARK_OFFSET)
+        }
+        super.touchesEnded(touches, with: event)
+    }
+
+}
