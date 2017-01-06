f15153736f00fc32294ca32f38a3211f09847adc
Luigi Donadel <luigi.donadel@gmail.com>
2017-01-06 19:54:39 +0100

    set current user on alpha vc

--- Trivia Patente/Trivia Patente/AlphaViewController.swift
+++ Trivia Patente/Trivia Patente/AlphaViewController.swift
@@ -12,6 +12,7 @@ class AlphaViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        (self.navigationController as! TPNavigationController).setUser(candidate: SessionManager.currentUser)
 
         // Do any additional setup after loading the view.
     }
