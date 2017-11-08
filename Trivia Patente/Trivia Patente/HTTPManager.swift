2e8d6c2c91f2f1c3236f6f550d5672cbb614fc12
Gabriel Ciulei <ciulei.gabriel@gmail.com>
2017-11-08 01:35:58 +0100

    added upload image to handle file uploads

--- Trivia Patente/Trivia Patente/HTTPManager.swift
+++ Trivia Patente/Trivia Patente/HTTPManager.swift
@@ -26,6 +26,55 @@ class HTTPManager {
         return headers
     }
     
+    func upload<T: TPResponse>(url : String, method : HTTPMethod, data: Data, forHttpParam: String, fileName: String = "",  mimeType: String, parameters: Parameters?, auth : Bool = true, handler: @escaping (T) -> Void)
+    {
+        let headers = HTTPManager.getAuthHeaders(auth: auth)
+        let destination = HTTPManager.getBaseURL() + url
+        
+        Alamofire.upload(multipartFormData: { multipartData in
+            multipartData.append(data, withName: forHttpParam, fileName: fileName, mimeType: mimeType)
+            if parameters != nil   {
+                for (key, value) in parameters! {
+                    multipartData.append((value as AnyObject).data(using: String.Encoding.utf8.rawValue)!, withName: key)
+                }
+            }
+        }, to: destination, headers: headers, encodingCompletion: { encodingResult in
+            switch encodingResult {
+            case .success(let upload, _, _):
+                upload.responseModel { (response : DataResponse<T>) in
+                    if let result = response.result.value {
+                        handler(result)
+                    } else if response.response == nil {
+                        let response = T(error: "Ci dispiace ma i server sono in manutenzione..\nRiprova tra un po\'! :/")
+                        handler(response)
+                    } else if let message = (response.result.error as? BackendError)?.message {
+                        let response = T(error: message, statusCode: response.response?.statusCode)
+                        if response.statusCode == 401 && auth {
+                            SessionManager.drop()
+                            UIViewController.goToFirstAccess()
+                        } else {
+                            handler(response)
+                        }
+                    } else {
+                        let response = T(error: "Errore sconosciuto. Riprova più tardi!")
+                        handler(response)
+                    }
+
+                }
+            case .failure(let encodingError):
+                print("encodingError:\(encodingError)")
+            }
+        })
+        
+        
+        
+//        upload(data, to: destination, method: method, headers: headers)
+//            .validate(statusCode: 200..<300)
+//            .validate(contentType: ["application/json"])
+//            .responseModel(completionHandler: { (response : DataResponse<T>) in
+//            })
+    }
+    
     func request<T: TPResponse>(url : String, method : HTTPMethod, params : Parameters?, auth : Bool = true, handler: @escaping (T) -> Void) {
         let headers = HTTPManager.getAuthHeaders(auth: auth)
         let destination = HTTPManager.getBaseURL() + url
