00d9b7c42e74859bef8a35454e610129ea6a8baa
Luigi Donadel <luigi.donadel@gmail.com>
2018-02-16 15:34:22 +0100

    Fixing unsafe access to currentUser

--- Trivia Patente/Trivia Patente/TPNavigationController.swift
+++ Trivia Patente/Trivia Patente/TPNavigationController.swift
@@ -99,8 +99,9 @@ class TPNavigationController: UINavigationController {
             if let currentUser = response.user {
                 SessionManager.set(user: currentUser)
                 UIImage.downloadImage(url: currentUser.avatarImageUrl, callback: { image in
-                    let u = SessionManager.currentUser! // need to create a copy to update values and the copy needs to be done inside the callback
-                    SessionManager.set(user: u)
+                    if let u = SessionManager.currentUser { // need to create a copy to update values and the copy needs to be done inside the callback
+                        SessionManager.set(user: u)
+                    }
                 })
             }
         }
